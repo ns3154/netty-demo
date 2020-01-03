56193e94f398dd25d6492ffbf6578fdf5c2f39bb
杨帮东 <397827222@qq.com>
2020-01-04 01:34:27 +0800

    first commit

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author 杨帮东 (qq:397827222)
+ * @version 1.0
+ * @date 2020/01/04 01:06
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
